1290a06a83592f135417cf24aa8e237892338a16
minsu <kms825252@gmail.com>
2022-11-16 13:32:53 +0900

    console 확인

--- final-pjt-front/src/store/index.js
+++ final-pjt-front/src/store/index.js
@@ -1,16 +1,33 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
+const API_URL = 'http://192.168.202.105:8000'
+
 export default new Vuex.Store({
   state: {
+    movies: [],
   },
   getters: {
   },
   mutations: {
   },
   actions: {
+    getMovies(context) {
+      axios({
+        method: 'get',
+        url: `${API_URL}/api/v1/movies/`,
+      })
+        .then((res) => {
+          console.log(res, context)
+          // context.commit('GET_MOVIES', res)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
   },
   modules: {
   }
