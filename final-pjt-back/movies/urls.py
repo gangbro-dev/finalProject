c4b5f46705c0b69ff24d10f21139b8f6c661e790
gangbro <gangbro.dev@gmail.com>
2022-11-21 14:28:11 +0900

    추천 알고리즘 완료

--- final-pjt-back/movies/urls.py
+++ final-pjt-back/movies/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.getMovieList),
     path('<int:movie_id>/', views.getMovieDetail),
     path('<int:movie_id>/comment/', views.comments),
-    path('<int:movie_id>/is_liked/', views.clickLikeButton)
+    path('<int:movie_id>/is_liked/', views.clickLikeButton),
+    path('recommend/', views.get_recommend_list),
 ]
